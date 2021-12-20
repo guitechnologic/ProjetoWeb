405bb3b6c56e1e6eb4c523b23149a2c321558384
guitechnologic <39844853+guitechnologic@users.noreply.github.com>
2021-12-20 17:01:54 -0300

    Json com Jquery no ajax

--- DevWebFull/13-PHP_AJAX/04-JsonComJqueryAjax/exercicio.php
+++ DevWebFull/13-PHP_AJAX/04-JsonComJqueryAjax/exercicio.php
@@ -2,13 +2,32 @@
 <html>
     <head>
         <meta charset="UTF-8">
+        <link href="_css/estilo.css" rel="stylesheet">
         <title>PHP com AJAX</title>
     </head>
 
     <body>
+        <button id="botao">carregar</button>
         <div id="listagem"></div>
         <script src="jquery.js"></script>
         <script>
+            $('button#botao').click(function() {
+                $('div#listagem').css('display', 'block');
+                carregarDados();
+            });
+            function carregarDados(){
+                $.getJSON('_json/produtos.json', function(data){
+                    var elemento;
+                    elemento = "<ul>";
+                    $.each(data, function(i, valor){
+                        elemento += "<li class='nome'>" + valor.nomeproduto + "</li>";
+                        elemento += "<li class='preco'>" + valor.precounitario + "</li>";
+                    });
+                    elemento += "</ul>";
+
+                    $('div#listagem').html(elemento);
+                });
+            }
 
         </script>
     </body>
