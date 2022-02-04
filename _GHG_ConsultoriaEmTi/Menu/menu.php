acf9b547d96c7774ea06d7551cbc7b08434f5d4f
guitechnologic <39844853+guitechnologic@users.noreply.github.com>
2022-02-04 18:04:40 -0300

    Atualização alisson, Javascript, etc

--- _GHG_ConsultoriaEmTi/Menu/menu.php
+++ _GHG_ConsultoriaEmTi/Menu/menu.php
@@ -9,30 +9,12 @@
             <meta   name="geo.placement"            content="Colombo">
             <meta   name="geo.position"             content="-25.36997746782584, -49.16598553283551">
             <meta   name="ICBM"                     content="-25.36997746782584, -49.16598553283551">
-            <link   rel="icon"                      href="_imagens/logovetorizadaghg/GHG - Consultoria em TI 2.png" sizes="16x16" type="image/png">
-            <link   rel="stylesheet"                href="_css/estilo.css">
+            <link   rel="icon"                      href="../_imagens/logovetorizadaghg/GHG - Consultoria em TI 2.png" sizes="16x16" type="image/png">
+            <link   rel="stylesheet"                href="../_css/estilo.css">
             <link   rel="stylesheet"                href="https://fonts.googleapis.com/css?family=Raleway">
             <link   rel="stylesheet"                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">   <!-- Icones Minimalistas -->
 
-        <style>
-
-            body,h1,h2,h3,h4,h5,h6  {font-family:   "Raleway",  sans-serif}
-
-            body,   html  {
-                height: 100%;
-                line-height:    1.8;
-                }
-
-
-            /* Barra de menu superior e botoes na barra */
-            .bar    .button    {
-                padding:    16px;
-            }
-
-        </style>
-
     <body>
-
         
         <div    class="top">
             <div    class="bar  white card" id="myNavbar">
@@ -42,7 +24,7 @@
                 <div    class="center    hide-small">
                     <a  href="#Home"                    class="bar-item button">        <i class="fa fa-home">      </i> HOME           </a>
                     <a  href="#servicos"                class="bar-item button">        <i class="fa fa-th">        </i> SERVIÇOS       </a>
-                    <a  href="../SobreNos/index.html"   class="bar-item button">        <i class="fa fa-user">      </i> SOBRE NÓS      </a>
+                    <a  href="SobreNos/index.html"      class="bar-item button">        <i class="fa fa-user">      </i> SOBRE NÓS      </a>
                     <a  href="#Contato"                 class="bar-item button">        <i class="fa fa-envelope">  </i> CONTATO        </a>
                     <a  href="indexEnglish.html"        class="bar-item button right">  <i class="fa fa-globe">     </i> ENGLISH        </a>
                 </div>
@@ -54,33 +36,15 @@
             </div>
         </div>
 
-        <!-- 01.3 - Configuração dos icones superiores, presentes na barra de navegação mobile -->
-        
+        <nav    class="sidebar  bar-block   black   card    animate-left    hide-medium hide-large" style="display:none"    id="mySidebar">
+            <a  href="javascript:void(0)"       onclick="botaoFechar()"    class="bar-item button  large   padding-16"> FECHAR</a>
+            <a  href="#Home"                    onclick="botaoFechar()"    class="bar-item button">    HOME         </a>
+            <a  href="#servicos"                onclick="botaoFechar()"    class="bar-item button">    SERVIÇOS     </a>
+            <a  href="SobreNos/index.html"      onclick="botaoFechar()"    class="bar-item button">    SOBRE NÓS    </a>
+            <a  href="#Contato"                 onclick="botaoFechar()"    class="bar-item button">    CONTATO      </a>
+            <a  href="indexEnglish.html"        onclick="botaoFechar()"    class="bar-item button">    ENGLISH      </a>
+        </nav>
 
-        
-        <!-- 13 - JavaScript -->
-        <script>
-
-
-        
-            // 13.1 - Alternancia entre mostrar e ocultar a barra lateral ao clicar no icone de menu
-            var mySidebar   =   document.getElementById(    "mySidebar" );
-
-            function    botaoAbre() {
-                if  (   mySidebar.style.display    ===  'block' )   {
-                    mySidebar.style.display =   'none'  ;
-                }   else    {
-                    mySidebar.style.display =   'block';
-                }
-            }
-
-            // 13.2 - Feche a barra lateral com o botão Fechar
-            function    botaoFechar()   {
-                mySidebar.style.display =   "none"  ;
-            }
-
-        </script>
-        
     </body>
 
 </html>
